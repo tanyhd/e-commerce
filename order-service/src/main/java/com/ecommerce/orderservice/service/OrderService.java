e128a8ee6864cba6b11a7f839f88e1f910e1c0f4
Darren <darren243@hotmail.com>
2023-03-07 21:30:20 +0800

    update order-service

--- order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
@@ -3,18 +3,15 @@ package com.ecommerce.orderservice.service;
 import com.ecommerce.orderservice.dto.InventoryResponse;
 import com.ecommerce.orderservice.dto.OrderLineItemsDto;
 import com.ecommerce.orderservice.dto.OrderRequest;
-import com.ecommerce.orderservice.external.inventoryService.InventoryServiceApi;
 import com.ecommerce.orderservice.model.Order;
 import com.ecommerce.orderservice.model.OrderLineItems;
 import com.ecommerce.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +19,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final InventoryServiceApi inventoryServiceApi;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -33,22 +30,26 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
+
         List<String> skuCodeList = orderLineItems.stream()
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        Map<String, List<String>> queryParam = new HashMap<>();
-        queryParam.put("skuCodeList", skuCodeList);
-        List<InventoryResponse> inventoryResponseList = inventoryServiceApi.getInventoryList(queryParam);
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCodeList", skuCodeList).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
 
-        boolean allProductsInStock = inventoryResponseList.stream()
-                .allMatch(InventoryResponse::isInStock);
-
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
         if (allProductsInStock) {
             orderRepository.save(order);
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
+
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
